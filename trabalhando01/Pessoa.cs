ead5ec1f4620370b535ef03f2b039093a639fd74
Israel Pereira <pereiraisraelcunha@gmail.com>
2021-05-06 18:09:37 -0300

    cmt_01
    commit referente ao trabalho do professor Gilson, sobre IMC

---
+++ trabalhando01/Pessoa.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalhando01
+{
+    class Pessoa
+    {
+        /*
+        https://classroom.google.com/u/1/c/MzAyNTM4Mjc0MDY1/m/MzMxNjU4MjM1OTY2/details
+        */
+
+        string nome, sexo;
+        double peso, altura;
+
+
+
+        //getter's and setter's
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public string Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+
+        public double metodoIMC (double peso, double altura)
+        {
+            double alturando = altura * altura;
+            return peso / alturando;
+        }
+
+
+        //classes
+        public double MassaBrancaNConfiavel (double peso, double altura)
+        {
+            //variaveis
+            double todaMassa, resultFinal;
+
+            //desenvolvimento
+            todaMassa = metodoIMC(peso, altura);
+            resultFinal = todaMassa - ((todaMassa * 30) / 100);
+
+            //resultado final
+            return resultFinal;
+        }//public double MassaBrancaNConfiavel (double peso, double altura)
+
+
+        public String RetornoClassificacaoIMC (double peso, double altura)
+        {
+            //variaveis
+            double todaMassa, regra01 = 0.00185, regra02 = 0.00249,
+                regra03 = 0.00299, regra04 = 0.00249, regra05 = 0.00349,
+                regra06 = 0.00399;
+            string resultFinal="";
+
+            //desenvolvimento & resposta
+            todaMassa = metodoIMC(peso, altura);
+                if (todaMassa < regra01) { resultFinal = "status..: abaixo do peso"; }
+                
+                else if ((todaMassa > regra01) && (todaMassa <= regra02))
+                { return resultFinal = " status..: peso normal";}
+            
+                else if ((todaMassa > regra02) && (todaMassa <= regra03))
+                { return resultFinal = " status..: sobrePeso";}
+            
+                else if ((todaMassa > regra03) && (todaMassa <= regra04))
+                { return resultFinal = " status..: obesidade grau 1"; }
+
+                else if ((todaMassa > regra04) && (todaMassa <= regra05))
+                { return resultFinal = " status..: obesidade grau 2"; }
+
+                else if ((todaMassa > regra05) && ((todaMassa <= regra06) || (todaMassa > regra06)))
+                { return resultFinal = " status..: obesidade grau 3"; }
+
+            return "";
+        }
+
+    }//class Pessoa
+}//namespace trabalhando01
---
+++ trabalhando01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace trabalhando01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_nome, user_sexo;
+            double user_peso, user_altura;
+
+            Pessoa pessoando_01 = new Pessoa();
+
+
+            //captar valores vindo do usuário
+            Console.WriteLine("qual seu nome? ");
+            user_nome = Console.ReadLine();
+
+            Console.WriteLine("qual seu sexo? ");
+            user_sexo = Console.ReadLine();
+
+            Console.WriteLine("qual seu peso? ");
+            user_peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("qual seu altura? ");
+            user_altura = Convert.ToDouble(Console.ReadLine());
+
+
+
+            //mostrar resultados
+            Console.Clear();
+            Console.WriteLine(" sr/srª " + user_nome + ", de sexo: " + user_sexo.ToLower()+"\n estas são as seguintes informações sobre seu corpo:");
+            Console.WriteLine(" seu IMC é de...: "+pessoando_01.metodoIMC(user_peso, user_altura));
+            Console.WriteLine(" sua massa não confiavel (30% do IMC) é de...: "+pessoando_01.MassaBrancaNConfiavel(user_peso, user_altura));
+            Console.WriteLine(pessoando_01.RetornoClassificacaoIMC(user_peso, user_altura));
+
+        }
+    }
+}
